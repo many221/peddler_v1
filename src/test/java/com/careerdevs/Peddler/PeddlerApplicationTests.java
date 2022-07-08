c9d6ccc7f89af8c34072ea67bc93a362d5386e76
Manny Lee <many221@gmail.com>
2022-07-07 17:49:12 -0700

    first commit

---
+++ src/test/java/com/careerdevs/Peddler/PeddlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.Peddler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeddlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
